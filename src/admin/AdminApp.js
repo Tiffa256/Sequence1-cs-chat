b9c72427d84fc72e3bdf3506742ece7bedaaeba7
Tiffa256 <tiffashan10@gmail.com>
2026-04-10 16:18:08 +0300

    Update AdminApp.js

--- src/admin/AdminApp.js
+++ src/admin/AdminApp.js
@@ -18,8 +18,8 @@ import "./Admin.css";
 const SESSION_KEY = "client_admin_authenticated_v1";
 
 // Hard-coded admin credentials (requested). WARNING: insecure in production.
-const ADMIN_USERNAME = "Sequence-commerce122";
-const ADMIN_PASSWORD = "Sequence-2026";
+const ADMIN_USERNAME = "Sequence2026";
+const ADMIN_PASSWORD = "Sequence@@2026";
 const DEFAULT_GLOBAL_PASSWORD = "Chat-with-us";
 const PROTECTED_ADMIN_URL = "https://sequence-cs-chat.onrender.com/admin";
 
